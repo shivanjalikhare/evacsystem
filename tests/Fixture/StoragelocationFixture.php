bd6fe720702f689d267c60987f5fd1514e43a53c
Shivanjali Khare <shivanjali.khare@gmail.com>
2017-11-15 12:42:09 -0600

    Added storagemarkers and storagelocation fixure

---
+++ tests/Fixture/StoragelocationFixture.php
@@ -0,0 +1,44 @@
+<?php
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * StoragelocationFixture
+ *
+ */
+class StoragelocationFixture extends TestFixture
+{
+
+    /**
+     * Fields
+     *
+     * @var array
+     */
+    // @codingStandardsIgnoreStart
+    public $fields = [
+        'id' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => false, 'default' => null, 'comment' => '', 'autoIncrement' => true, 'precision' => null],
+        'city' => ['type' => 'string', 'length' => 22, 'null' => false, 'default' => null, 'collate' => 'latin1_swedish_ci', 'comment' => '', 'precision' => null, 'fixed' => null],
+        '_constraints' => [
+            'primary' => ['type' => 'primary', 'columns' => ['id'], 'length' => []],
+            'city' => ['type' => 'unique', 'columns' => ['city'], 'length' => []],
+        ],
+        '_options' => [
+            'engine' => 'InnoDB',
+            'collation' => 'latin1_swedish_ci'
+        ],
+    ];
+    // @codingStandardsIgnoreEnd
+
+    /**
+     * Records
+     *
+     * @var array
+     */
+    public $records = [
+        [
+            'id' => 1,
+            'city' => 'Lafayette'
+        ],
+    ];
+}
