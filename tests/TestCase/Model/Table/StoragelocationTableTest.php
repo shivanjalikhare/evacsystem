35e355ccdc9b09c9fa6fe0a61d9965ed0dc536a4
Shivanjali Khare <shivanjali.khare@gmail.com>
2017-11-15 12:36:14 -0600

    Added StorageMarkers and StorageLocation tabletest

---
+++ tests/TestCase/Model/Table/StoragelocationTableTest.php
@@ -0,0 +1,113 @@
+<?php
+namespace App\Test\TestCase\Model\Table;
+
+use App\Model\Table\StoragelocationTable;
+use Cake\ORM\TableRegistry;
+use Cake\TestSuite\TestCase;
+
+/**
+ * App\Model\Table\Storagelocation Test Case
+ */
+class StoragelocationsTableTest extends TestCase
+{
+
+    /**
+     * Test subject
+     *
+     * @var \App\Model\Table\Storagelocation
+     */
+    public $Storagelocation;
+
+    /**
+     * Fixtures
+     *
+     * @var array
+     */
+    public $fixtures = [
+        'app.storagelocation'
+    ];
+
+    /**
+     * setUp method
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        $config = TableRegistry::exists('Storagelocation') ? [] : ['className' => StoragelocationTable::class];
+        $this->Storagelocation = TableRegistry::get('Storagelocation', $config);
+    }
+
+    /**
+     * tearDown method
+     *
+     * @return void
+     */
+    public function tearDown()
+    {
+        unset($this->Storagelocation);
+
+        parent::tearDown();
+    }
+
+    /**
+     * Test initialize method
+     *
+     * @return void
+     */
+    public function testInitialize()
+    {
+        $this->Storagelocation->initialize([]); //have to call manually to get coverage.
+        $this->assertEquals(
+            'id',
+            $this->Storagelocation->primaryKey(),
+            'The [Users]Table default primary key is expected to be `id`.'
+        );
+    }
+
+    /**
+     * Test validationDefault method
+     *
+     * @return void
+     */
+    public function testValidationDefault()
+    {
+        $data = [
+            'id' => 1,
+            'city' => 'Lafayette'
+        ];
+
+        $storagelocation = $this->Storagelocation->newEntity($data);
+        $this->assertEmpty($storagelocation->errors());
+    }
+
+    /**
+     * Test validationDefault method
+     *
+     * @return void
+     */
+    public function testValidationDefault_CheckFields() {
+        $validator = new \Cake\Validation\Validator(); //object
+        $validator = $this->Storagelocation->validationDefault($validator);
+        $this->assertTrue($validator->hasField('id'));
+        $this->assertTrue($validator->hasField('city'));
+         
+    }
+
+    /**
+     * Test buildRules method
+     *
+     * @return void
+     */
+    public function testBuildRules()
+    {
+        $comment =  $this->Storagelocation->newEntity([
+            'city' => 'Lafayette'
+            
+        ]);
+
+        $result = $this->Storagelocation->checkRules($comment);
+        $this->assertFalse($result);
+    }
+}
---
+++ tests/TestCase/Model/Table/StoragemarkersTableTest.php
@@ -0,0 +1,110 @@
+<?php
+namespace App\Test\TestCase\Model\Table;
+
+use App\Model\Table\StoragemarkersTable;
+use Cake\ORM\TableRegistry;
+use Cake\TestSuite\TestCase;
+
+/**
+ * App\Model\Table\StoragemarkersTable Test Case
+ */
+class StoragemarkersTableTest extends TestCase
+{
+
+    /**
+     * Test subject
+     *
+     * @var \App\Model\Table\StoragemarkersTable
+     */
+    public $Storagemarkers;
+
+    /**
+     * Fixtures
+     *
+     * @var array
+     */
+    public $fixtures = [
+        'app.storagemarkers'
+    ];
+
+    /**
+     * setUp method
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        $config = TableRegistry::exists('Storagemarkers') ? [] : ['className' => StoragemarkersTable::class];
+        $this->Storagemarkers = TableRegistry::get('Storagemarkers', $config);
+    }
+
+    /**
+     * tearDown method
+     *
+     * @return void
+     */
+    public function tearDown()
+    {
+        unset($this->Storagemarkers);
+
+        parent::tearDown();
+    }
+
+    /**
+     * Test initialize method
+     *
+     * @return void
+     */
+    public function testInitialize()
+    {
+         
+        $this->Storagemarkers->initialize([]); //have to call manually to get coverage.
+        $this->assertEquals(
+            'id',
+            $this->Storagemarkers->primaryKey(),
+            'The [Users]Table default primary key is expected to be `id`.'
+        );
+
+        
+    }
+
+    /**
+     * Test validationDefault method
+     *
+     * @return void
+     */
+    public function testValidationDefault()
+    {
+       $data = [
+        'id' => 1,
+            'name' => 'Lafayette',
+            'address' => '2000 Johnston Street',
+            'lat' => 1,
+            'lng' => 1,
+            'type' => 'Storage'
+        ];
+
+        $storagemarkers = $this->Storagemarkers->newEntity($data);
+        $this->assertEmpty($storagemarkers->errors());
+    }
+
+    /**
+     * Test validationDefault method
+     *
+     * @return void
+     */
+    public function testValidationDefault_CheckFields() {
+        
+        $validator = new \Cake\Validation\Validator(); //object
+
+        $validator = $this->Storagemarkers->validationDefault($validator);
+
+        $this->assertTrue($validator->hasField('id'));
+        $this->assertTrue($validator->hasField('name'));
+        $this->assertTrue($validator->hasField('address'));
+        $this->assertTrue($validator->hasField('lat')); 
+        $this->assertTrue($validator->hasField('lng'));
+        $this->assertTrue($validator->hasField('type'));  
+    }
+}
