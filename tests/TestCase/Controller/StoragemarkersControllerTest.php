331d66058bce94d6e5bd113d36ec1199c518a0c2
Razin Farhan Hussain <razin.2good@gmail.com>
2017-11-15 14:52:05 -0600

    Storage Marker Controller Test Added

---
+++ tests/TestCase/Controller/StoragemarkersControllerTest.php
@@ -0,0 +1,49 @@
+<?php
+namespace App\Test\TestCase\Controller;
+
+use App\Controller\StoragemarkersController;
+use Cake\TestSuite\IntegrationTestCase;
+
+/**
+ * App\Controller\StoragemarkersController Test Case
+ */
+class StoragemarkersControllerTest extends IntegrationTestCase
+{
+
+    /**
+     * Fixtures
+     *
+     * @var array
+     */
+    public $fixtures = [
+        'app.storagemarkers', 'app.users'
+    ];
+
+    /**
+     * Test view method
+     *
+     * @return void
+     */
+    public function testView()
+    {
+        // Set session data
+        $this->session([
+        'Auth' => [
+            'User' => [
+                'id'=> 1,
+            ]
+        ]
+        ]);
+
+        //$this->get('/markers');
+        $this->get('/users'); //get the list of users
+        $this->assertSession(1, 'Auth.User.id'); //check if the user is logged in
+
+        $this->get('/storagemarkers/view');
+
+        $this->assertResponseOk();
+             
+
+    }
+
+}
