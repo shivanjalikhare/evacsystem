f9c2dc59c5101fb1c0971ccbe07fc06635822529
Razin Farhan Hussain <razin.2good@gmail.com>
2017-11-27 23:24:01 -0600

    Organization Controller test - 3 TC added

--- tests/TestCase/Controller/OrganizationControllerTest.php
+++ tests/TestCase/Controller/OrganizationControllerTest.php
@@ -15,9 +15,7 @@ class OrganizationControllerTest extends IntegrationTestCase
      *
      * @var array
      */
-    public $fixtures = [
-        'app.organization'
-    ];
+    public $fixtures = ['app.organization'];
 
     /**
      * Test index method
@@ -26,7 +24,19 @@ class OrganizationControllerTest extends IntegrationTestCase
      */
     public function testIndex()
     {
-        $this->markTestIncomplete('Not implemented yet.');
+        //$this->markTestIncomplete('Not implemented yet.');
+         $this->session([ //mock user session
+        'Auth' => [
+            'User' => [
+                'id' => 1,
+                // other keys.
+            ]
+        ]
+        ]);
+    
+        $this->get('/Volunteer/organization');
+        // Check that the response was a 200
+        $this->assertResponseOk();
     }
 
     /**
@@ -36,7 +46,19 @@ class OrganizationControllerTest extends IntegrationTestCase
      */
     public function testView()
     {
-        $this->markTestIncomplete('Not implemented yet.');
+        //$this->markTestIncomplete('Not implemented yet.');
+        $this->session([ //mock user session
+        'Auth' => [
+            'User' => [
+                'id' => 1,
+                // other keys.
+            ]
+        ]
+        ]);
+    
+        $this->get('/organization/view/1');
+        // Check that the response was a 200
+        $this->assertResponseOk();
     }
 
     /**
@@ -46,7 +68,7 @@ class OrganizationControllerTest extends IntegrationTestCase
      */
     public function testAdd()
     {
-        $this->markTestIncomplete('Not implemented yet.');
+        $this->markTestIncomplete('Not used in project.');
     }
 
     /**
@@ -56,7 +78,7 @@ class OrganizationControllerTest extends IntegrationTestCase
      */
     public function testEdit()
     {
-        $this->markTestIncomplete('Not implemented yet.');
+        $this->markTestIncomplete('Not used in project.');
     }
 
     /**
@@ -66,6 +88,32 @@ class OrganizationControllerTest extends IntegrationTestCase
      */
     public function testDelete()
     {
-        $this->markTestIncomplete('Not implemented yet.');
+        $this->markTestIncomplete('Not used in project.');
+    }
+
+    /**
+     * Test send email method
+     *
+     * @return void
+     */
+    public function testsendEmailtoOrganization()
+    {
+        //$this->markTestIncomplete('Not implemented yet.');
+        $this->enableRetainFlashMessages();
+        $this->session([ //mock user session
+            'Auth' => [
+                'User' => [
+                'id' => 1,
+                // other keys.
+                ]
+            ]
+        ]);
+        $this->get('/users'); //get the list of users
+        $this->assertSession(1, 'Auth.User.id'); //check if the user is logged in
+
+        $emailObject = new OrganizationController();
+        $emailObject->sendEmail('razin2good@gmail.com');
+        $this->assertSession('Email sent.', 'Flash.flash.0.message');
+
     }
 }
