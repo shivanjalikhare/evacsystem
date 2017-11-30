41ce138fd864aeec0958f7c565ff19db40683bf7
Razin Farhan Hussain <razin.2good@gmail.com>
2017-11-29 20:03:13 -0600

    Orgteam table test - 5 TC added and Orgteam fixture added

--- tests/TestCase/Model/Table/OrgteamTableTest.php
+++ tests/TestCase/Model/Table/OrgteamTableTest.php
@@ -58,7 +58,12 @@ class OrgteamTableTest extends TestCase
      */
     public function testInitialize()
     {
-        $this->markTestIncomplete('Not implemented yet.');
+        $this->Orgteam->initialize([]); //have to call manually to get coverage.
+        $this->assertEquals(
+            'teamid',
+            $this->Orgteam->primaryKey(),
+            'The [Orgteam]Table default primary key is expected to be `teamid`.'
+        );
     }
 
     /**
@@ -68,6 +73,58 @@ class OrgteamTableTest extends TestCase
      */
     public function testValidationDefault()
     {
-        $this->markTestIncomplete('Not implemented yet.');
+        //checking all the fields are given for creating new entity
+        $data = [
+            'teamid' => 1,
+            'teamname' => 'Evac1',
+            'members' => 3,
+            'details' => 'Razin, Shan, Arman',
+            'contact' => 'razinfh@outlook.com'
+        ];
+
+
+        $user = $this->Orgteam->newEntity($data);
+        $this->assertEmpty($user->errors());
     }
+
+    /**
+     * Test validationDefault method
+     *
+     * @return void
+     */
+    public function testValidationDefault_CheckFields() {
+        $validator = new \Cake\Validation\Validator(); //object
+        $validator = $this->Orgteam->validationDefault($validator);
+        $this->assertTrue($validator->hasField('teamid'));
+        $this->assertTrue($validator->hasField('teamname'));
+        $this->assertTrue($validator->hasField('members'));
+        $this->assertTrue($validator->hasField('details'));
+        $this->assertTrue($validator->hasField('contact'));   
+    }
+
+    /**
+     * Test fixture records of similar type
+     *
+     * @return void
+     */
+     public function testFixtureRecordExits()
+     {
+       
+        $data = $this->Orgteam->find()->where(['contact' => 'razin2good@gmail.com']);
+        $this->assertEquals(1, $data->count());
+
+     }
+
+      /**
+     * Test fixture records of similar type
+     *
+     * @return void
+     */
+     public function testFixtureRecordsWithSameType()
+     {
+       
+        $data = $this->Orgteam->find()->where(['teamname' => 'Evac1']);
+        $this->assertEquals(1, $data->count());
+
+     }
 }
